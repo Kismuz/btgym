30db3181fec91779b4b2b5e170f0e87ff27a7f72
Kismuz <muzikinae@gmail.com>
2017-05-25 17:26:34 +0300

    addet project outline to readme

--- README.md
+++ README.md
@@ -11,7 +11,76 @@ http://github.com/openai/gym
 
 The project in early pre-pre-alpha stage and you are likely to only find chunks of code and notebooks at the moment.
 
+OUTLINE:
 
+Proposed data flow:
+
+            BacktraderEnv                                  RL alorithm
+                                           +-+
+   (episode mode)  +<------<action>------->| |<--------------------------------------+
+          |        |                       |e|                                       |
+          +<------>+-------<state >------->|n|--->[feature  ]---><state>--+->[agent]-+
+          |        |       <matrix>        |v|    [estimator]             |     |
+          |        |                       |.|                            |     |
+    [Backtrader]   +-------<portfolio >--->|s|--->[reward   ]---><reward>-+     |
+    [Server    ]   |       <statistics>    |t|    [estimator]                   |
+       |           |                       |e|                                  |
+       |           +-------<is_done>------>|p|--+>[runner]<-------------------->+
+  (control mode)   |                       | |  |    |
+       |           +-------<aux.info>----->| |--+    |
+       |                                   +-+       |
+       +--<'stop'><-------------------->|env.close|--+
+       |                                             |
+       +--<'reset'><------------------->|env.reset|--+
+
+
+ Notes:
+ 1. While feature estimator and 'MDP state composer' are traditionally parts of RL algoritm,
+    reward estimation is often performed inside environment. In case of portfolio optimistion
+    reward function can be tricky, so it is reasonable to make it easyly accessable inside RL algorithm,
+    computed by [reward estimator] module and based on some set of portfolio statisics.
+ 2. [state matrix], returned by Environment is 2d [m,n] array of floats, where m - number of Backtrader
+    Datafeed values: v[-n], v[-n+1], v[-n+2],...,v[0] i.e. from present step to n steps back, and
+    every v[i] is itself a vector of m features (open, close,...,volume,..., mov.avg., etc.).
+    - in case of n=1 process is obviously POMDP. Ensure MDP property by 'frame stacking' or/and
+      employing reccurent function approximators. When n>>1 process [somehow] approaches MDP (by means of
+      Takens' delay embedding theorem).
+    - features are defined by WorkHorseStrategy.next() method,
+      wich itself lives inside bt_server_process() function, and can be customised as needed.
+    - same holds for portfolio statistics.
+    <<TODO: pass features and stats as parameters of environment>>
+ 3. Action space is discrete with basic actions: 'buy', 'sell', 'hold', 'close',
+    and control action 'done' for early epidsode termination.
+    <<!:very incomplete: order amounts? ordering logic not defined>>
+ 4. This environment is meant to be [not nessesserily] paired with Tensorforce RL library,
+    that's where [runner] module came from.
+
+ 5. Why Gym, not Universe VNC environment?
+    For algorithmic trading, clearly, vnc-type environment fits much better.
+    But to the best of my knowledge, OpenAI yet to publish docs on custom Universe VNC environment creation.
+
+ 6. Why Backtrader library, not Zipline/PyAlgotrader etc.?
+    Those are excellent platforms, but what I really like about Backtrader is appealing programming logic and
+    ease of customisation. You dont't need to do tricks, say, to disable automatic calendar fetching as with Zipline.
+    I mean, it's very convinient for people but prevents from feeding forex data. IMO Backtrader is simply better suited
+    for this kind of experiment.
+
+ 7. Why Forex data?
+    Obviously environment is data/market agnostic. Backtesting dataset size is what matters. Deep Q-value algorithms,
+    sample efficient among deep RL, take 1M steps just to lift off. 1 year 1 minute FX data contains about 300K samples.
+    Feeding several years of data makes it realistic to expect algorithm to converge for intraday trading i.e.
+    1000-1500 steps per episode. That's just preliminary experiment setup, not proved!
+
+SERVER OPERATION:
+Backtrader server starts when BacktraderEnv is instantiated, runs as separate process, follows Request/Reply pattern
+(every request should be paired with reply message) and operates one of two modes:
+1. Control mode: initial mode, accepts only 'reset' and 'stop' messages. Any other message is ignored and replied with
+   standart info messge. Shuts down upon recieving 'stop' via environment close() method,
+   goes to episode mode upon 'reset' (via env.rest()).
+2. Episode mode: runs episode following WorkHorseStrategy logic and parameters. Accepts <action> messages,
+   returns tuple <[state matr.], [portf.stats], [is_done], [aux.info]>.
+   Finishes episode upon recieving <action>='done' or according to Backtrader strategy logic, falls
+   back to control mode.
 
 
 
--- gym_bt/envs/bt_env.py
+++ gym_bt/envs/bt_env.py
@@ -18,6 +18,293 @@
 ###############################################################################
 
 
-#
-# #####
-#
+import multiprocessing
+import time
+import datetime
+import zmq
+
+import gym
+from gym import error, spaces
+from gym import utils
+from gym.utils import seeding, closer
+
+import numpy as np
+
+import logging
+logger = logging.getLogger(__name__)
+
+import backtrader as bt
+import backtrader.feeds as btfeeds
+import pandas
+
+
+class BacktraderEnv(gym.Env, ):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self,
+                 datafilename='./DAT_ASCII_EURUSD_M1_201702.csv',
+                 net_address='tcp://127.0.0.1:5500',
+                 max_episode_len=1000):
+        # Server/network parameters:
+        self.server = None
+        self.network_address = net_address
+        self.client_socket = None
+        # RL env. related parameters:
+        self.max_episode_len = max_episode_len
+        self.observation_space = None
+        self.action_space = spaces.Discrete(3)
+        # Backtrader related parameters:
+        self.cerebro_params = None
+        # CSV parsing parameters:
+        # These are specific to generic ASCII M1 bars FOREX data
+        # from HistData.com
+        self.datafeed_params = (
+            ('dataname', datafilename),
+            ('nullvalue', 0.0),
+            ('dtformat', '%Y%m%d %H%M%S'),
+            ('headers', True),
+            ('separator', ';'),
+            ('datetime', 0),
+            ('high', 1),
+            ('low', 2),
+            ('open', 3),
+            ('close', 4),
+            ('volume', 5),
+            ('openinterest', -1))
+        # Run Backtrader server
+        self._start_server()
+
+    def _start_server(self):
+        """
+        Starts backtrader REQ/REP server as separate process
+        and opens client comm. socket
+        """
+        # DBG
+        print('Start(): starting server, max episode length:', self.max_episode_len)
+        # Start server process
+        self.server = multiprocessing.Process(target=bt_server_process,
+                                              args=(self.datafeed_params,
+                                                    self.max_episode_len,
+                                                    self.cerebro_params,
+                                                    self.network_address))
+        self.server.daemon = False
+        self.server.start()
+        # Wait for server to startup
+        time.sleep(5)
+        # DBG
+        print('Start(): server started with pid=', self.server.pid)
+        # Set up client channel:
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REQ)
+        self.socket.connect(self.network_address)
+        # DBG
+        print('Start(): client socket connected @ {}, pinging Backtrader:'.format(self.network_address))
+        self.socket.send_pyobj('ping!')
+        self.control_response = self.socket.recv_pyobj()
+        print('Start(): received response: <{}>'.format(self.control_response))
+
+    def _reset(self):
+        """
+        Implementation of OpenAI env.reset method.
+        Rewinds backtrader server and starts new episode.
+        Returns initial environment observations.
+        """
+        self.socket.send_pyobj('reset')
+        self.control_response = self.socket.recv_pyobj()
+        return self.control_response
+
+    def _step(self, action):
+        """
+        Implementation of OpenAI env.step method.
+        Relies on remote backtrader server for actual environment dynamics computing
+        """
+        # DBG
+        print('Step(): sending action', action)
+        # Send action to engine
+        self.socket.send_pyobj(action)
+        # Recieve response
+        self.step_response = self.socket.recv_pyobj()
+        # DBG
+        print('Step(): recieved response {} as {}'.format(self.step_response, type(self.step_response)))
+        return self.step_response
+
+    def _compute_reward(self):
+        """
+        Computes RL reward based on backtrader current portfolio estimates.
+        Actual reward function depends on how 'performance' is defined.
+        """
+        # TODO: not implemented
+        self.reward = 0  # = TrickyRewardFunction[self.step_response]
+
+        return self.reward
+
+    def _close(self):
+        """
+        Shuts BT server down, destroys comm. chanell
+        """
+        self.socket.send_pyobj('stop')
+        self.control_response = self.socket.recv_pyobj()
+        # Ensure server exited:
+        time.sleep(5)
+        # Why take chances?
+        self.server.terminate()
+        self.server.join()
+        self.socket.disconnect()
+        self.context.destroy()
+        return self.control_response
+
+
+def bt_server_process(dataparams,
+                      max_steps,
+                      cerebro_params,
+                      network_address):
+    """
+    Backtrader server.
+    Control signals:
+    IN:
+    'reset' - rewinds backtrader engine and runs new episode;
+    'stop' - server shut-down.
+    OUT:
+    info: <string message> - reports current server status.
+    Within-episode signals:
+    IN:
+    <integer> - encoded action;
+    'done' - stops current episode.
+    OUT:
+    response - <dict>: observation - observation of the current environment 
+                                as [m,n] array of <fl32>, where:
+                                n - num. of last datafeed values,
+                                m - num. of data features;
+                       reward - current portfolio statistics for environment reward signal computing;
+                       done - episode termination flag;
+                       info - auxiliary diagnostic information, if any.
+
+    Parameters:
+    dataparms - CSV file name and parsing parameters;
+    max_steps - <int>, maximum episode length;
+    network_address - <str>, network address to bind to;
+    cerebro_params - <dict>: trading engine-specific parameters, excl. datafeed 
+    """
+
+    class FxCSVData(btfeeds.GenericCSVData):
+        """
+        Backtrader CSV datafeed class
+        """
+        params = dataparams
+
+    class WorkHorseStrategy(bt.Strategy):
+        """
+        Defines actual environment inner dynamics.
+        """
+        params = (('socket', None),
+                  ('max_steps', 0),
+                  ('process', None),
+                  ('iterations', 0),)
+
+        def __init__(self):
+            # Set parameters using args passed to bt_server_process
+            self.p.process = multiprocessing.current_process()
+            self.p.max_steps = max_steps
+            print(info('process pid='), self.process.pid)
+
+        def prenext(self):
+            print('prenext() envoked.')
+
+        def episode_stop(self):
+            """
+            Surprisingly, stops current episode 
+            """
+            self.env.runstop()
+
+        def next(self):
+            """
+            Defines one step environment routine
+            """
+            is_done = False
+            self.p.iterations += 1
+            # Is it last step of the episode?
+            if self.p.iterations >= self.p.max_steps:
+                is_done = True
+                # DBG
+            print('Server: waiting for input')
+            action_input = self.p.socket.recv_pyobj()
+            # DBG
+            print('Server: recieved {} as {}'.format(action_input, type(action_input)))
+            # If client forces to finish episode:
+            if action_input == 'done':
+                action_input = None  # Some predefined action e.g. <close all positions>
+                is_done = True
+
+            # Do some mindfull computing...
+            # is_done flag can also be rised here by
+            # trading stimulator event, e.g. <OMG, we became too rich!>
+            time.sleep(1)
+            # STATE
+            # REWARD
+            # INFO
+            # Compose and send response
+            response = {'state': 'STATE', 'reward': 0, 'done': is_done, 'info': 'no information'}
+            self.p.socket.send_pyobj(response)
+            # DBG
+            print('Server: response sent.')
+
+            if is_done: self.episode_stop()
+
+    def info(message):
+        # Look who's talking
+        return 'BT server: ' + message
+
+        #### MAIN bt_server_process:
+
+    # The best is yet to come:
+    wonderful_results = []
+    some_important_statisitc = []
+    # Set up a comm. channel for server as zmq socket
+    # to carry both service and data signal
+    # !! Reminder: Since we use REQ/REP - messages do go in pairs !!
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind(network_address)
+
+    # Main loop
+    while True:  # TODO: use itertools here -> define global step
+        # Server is in 'control' mode:
+        service_input = socket.recv_pyobj()
+        while service_input != 'reset':
+            print(info('waiting for reset/stop signal'))
+            socket.send_pyobj(info('waiting for reset/stop signal'))
+            service_input = socket.recv_pyobj()
+            # Check if we done with training:
+            if service_input == 'stop':
+                # Release comm channel, gather statistic and exit:
+                # TODO: Gather overall statistics
+                # all the server shutdown logic is here
+                some_important_statisitc = True
+                # To leave gracefully:
+                print(info('exiting.'))
+                socket.send_pyobj(info('exiting.'))
+                socket.close()
+                context.destroy()
+                return wonderful_results, some_important_statisitc
+                # And where do you think you actually return it, hah?  <-- TODO: dump stats to file or something
+
+        # Got 'reset'? --> start new episode:
+        print(info('starting new episode'))
+        socket.send_pyobj(info('starting new episode'))  # just to pair control message
+
+        # <some fancy code to define random data entry point for upcoming episode>
+        # <i.e. 'fromdate'  and 'todate' parameters>
+
+        datafeed = FxCSVData(fromdate=datetime.datetime(2017, 2, 1),
+                             todate=datetime.datetime(2017, 2, 2), )
+        # Compose bt.cerebro class for the episode:
+        cerebro = bt.Cerebro(stdstats=False)
+        cerebro.addstrategy(WorkHorseStrategy,
+                            socket=socket,
+                            iterations=0)
+        cerebro.adddata(datafeed)
+        wonderful_results = cerebro.run()
+
+    # Just in case -- we actually cannot get there except by some ignored exception
+    return wonderful_results, some_important_statisitc
+
