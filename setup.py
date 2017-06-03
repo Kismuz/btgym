882159fd47fb34fdf82bdf0f32dd66dd0af0b248
Kismuz <muzikinae@gmail.com>
2017-06-03 22:17:58 +0300

    added .gitignore

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ from setuptools import setup
 
 setup(name='gym_bt',
       version='0.0.1',
-      install_requires=['gym', 'backtrader[plotting]', 'pyzmq']
+      install_requires=['gym', 'backtrader[plotting]', 'pyzmq', 'numpy']
 )  
