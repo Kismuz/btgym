in progress; ep.stat averaging fixed, purge previos logs added

btgym/algorithms/aac.py
@@ -453,7 +453,7 @@ class BaseAAC(object):
             self.ep_summary = dict(
                 # Summary placeholders
                 render_human=tf.placeholder(tf.uint8, [None, None, None, 3]),
-                render_model_input=tf.placeholder(tf.uint8, [None, None, None, 3]),
+                render_model_input_ext=tf.placeholder(tf.uint8, [None, None, None, 3]),
                 render_episode=tf.placeholder(tf.uint8, [None, None, None, 3]),
                 render_atari=tf.placeholder(tf.uint8, [None, None, None, 1]),
                 total_r=tf.placeholder(tf.float32, ),
@@ -465,7 +465,7 @@ class BaseAAC(object):
             self.ep_summary['render_op'] = tf.summary.merge(
                 [
                     tf.summary.image('human', self.ep_summary['render_human']),
-                    tf.summary.image('model_input', self.ep_summary['render_model_input']),
+                    tf.summary.image('model_input_external', self.ep_summary['render_model_input_ext']),
                     tf.summary.image('episode', self.ep_summary['render_episode']),
                 ],
                 name='render'
@@ -804,6 +804,7 @@ class BaseAAC(object):
 
         # Every worker writes episode and model summaries:
         ep_summary_feeder = {}
+
         # Collect episode summaries from all env runners:
         for stat in data['ep_summary']:
             if stat is not None:
@@ -817,6 +818,7 @@ class BaseAAC(object):
             ep_summary_feed_dict = {
                 self.ep_summary[key]: np.average(list) for key, list in ep_summary_feeder.items()
             }
+
             if self.test_mode:
                 # Atari:
                 fetched_episode_stat = sess.run(self.ep_summary['test_stat_op'], ep_summary_feed_dict)

btgym/algorithms/launcher.py
@@ -30,6 +30,7 @@ import os
 import logging
 import time
 import psutil
+import glob
 from subprocess import PIPE
 import signal
 import numpy as np
@@ -56,6 +57,7 @@ class Launcher():
                  max_env_steps=None,
                  root_random_seed=None,
                  test_mode=False,
+                 purge_previous=0,
                  verbose=0):
         """
 
@@ -68,6 +70,8 @@ class Launcher():
             max_env_steps:      total number of environment steps to run training on
             root_random_seed:   int or None
             test_mode:          if True - use Atari gym env., BTGym otherwise.
+            purge_previous:     int, keep or remove previous log files and saved checkpoints from log_dir, if found:
+                                0 - keep, 1 - ask, 2 - remove
             verbose:            0 - silent, 1 - info, 3 - debug level
 
         Note:
@@ -114,6 +118,7 @@ class Launcher():
         self.max_env_steps = 100 * 10 ** 6
         self.ports_to_use = []
         self.root_random_seed = root_random_seed
+        self.purge_previous = purge_previous
         self.test_mode = test_mode
         self.verbose = verbose
 
@@ -148,9 +153,24 @@ class Launcher():
             np.random.randint(0, 2**30, self.cluster_config['num_workers'] + self.cluster_config['num_ps'])
         )
 
-        if not os.path.exists(self.cluster_config['log_dir']):
+        # Log_dir:
+        if os.path.exists(self.cluster_config['log_dir']):
+            # Remove previous log files and saved model if opted:
+            if self.purge_previous > 0:
+                confirm = 'y'
+                if self.purge_previous < 2:
+                    confirm = input('<{}> already exists. Override[y/n]?'.format(self.cluster_config['log_dir']))
+                if confirm in 'y':
+                    files = glob.glob(self.cluster_config['log_dir'] + '/*')
+                    p = psutil.Popen(['rm', '-R', ] + files, stdout=PIPE, stderr=PIPE)
+                    self.log.warning('Files in <{}> purged.'.format(self.cluster_config['log_dir']))
+
+            else:
+                self.log.warning('Appending to <{}>.'.format(self.cluster_config['log_dir']))
+
+        else:
             os.makedirs(self.cluster_config['log_dir'])
-            self.log.info('{} created.'.format(self.cluster_config['log_dir']))
+            self.log.warning('<{}> created.'.format(self.cluster_config['log_dir']))
 
         for kwarg in ['port', 'data_port']:
             assert kwarg in self.env_config['kwargs'].keys()
@@ -165,6 +185,16 @@ class Launcher():
         env_ports = np.arange(self.cluster_config['num_envs'])
         worker_port = self.env_config['kwargs']['port']  # start value for BTGym comm. port
 
+        #for k, v in self.env_config['kwargs'].items():
+        #    try:
+        #        c = copy.deepcopy(v)
+        #        print('key: {} -- deepcopy ok.'.format(k))
+        #    except:
+        #        print('key: {} -- deepcopy failed!.'.format(k))
+
+        # TODO: Hacky, cause dataset is threadlocked; do pass dataset as class_ref + kwargs_dict:
+        dataset_instance = self.env_config['kwargs'].pop('dataset')
+
         for key, spec_list in self.cluster_spec.items():
             task_index = 0  # referenced further as worker id
             for _id in spec_list:
@@ -174,6 +204,8 @@ class Launcher():
                     # Configure worker BTgym environment:
                     if task_index == 0:
                         env_config['kwargs']['data_master'] = True  # set worker_0 as chief and data_master
+                        env_config['kwargs']['dataset'] = dataset_instance
+                        env_config['kwargs']['render_enabled'] = True
                     else:
                         env_config['kwargs']['data_master'] = False
                         env_config['kwargs']['render_enabled'] = False  # disable rendering for all but chief
@@ -228,15 +260,19 @@ class Launcher():
         cluster['worker'] = all_workers
         return cluster
 
-    def clear_port(self, port):
+    def clear_port(self, port_list):
         """
-        Kills process on specified port, if any.
+        Kills process on specified ports list, if any.
         """
-        p = psutil.Popen(['lsof', '-i:{}'.format(port), '-t'], stdout=PIPE, stderr=PIPE)
-        pid = p.communicate()[0].decode()[:-1]  # retrieving PID
-        if pid is not '':
-            p = psutil.Popen(['kill', pid])
-            self.log.info('port {} cleared'.format(port))
+        if not isinstance(port_list, list):
+            port_list = [port_list]
+
+        for port in port_list:
+            p = psutil.Popen(['lsof', '-i:{}'.format(port), '-t'], stdout=PIPE, stderr=PIPE)
+            pid = p.communicate()[0].decode()[:-1]  # retrieving PID
+            if pid is not '':
+                p = psutil.Popen(['kill', pid])
+                self.log.info('port {} cleared'.format(port))
 
     def _update_config_dict(self, old_dict, new_dict=None):
         """

btgym/algorithms/nnet_util.py
@@ -131,10 +131,10 @@ def conv1d(x, num_filters, name, filter_size=3, stride=2, pad="SAME", dtype=tf.f
 def conv_2d_network(x,
                     ob_space,
                     ac_space,
-                    num_layers=4,
-                    num_filters=32,
-                    filter_size=(3, 3),
-                    stride=(2, 2),
+                    conv_2d_num_layers=4,
+                    conv_2d_num_filters=32,
+                    conv_2d_filter_size=(3, 3),
+                    conv_2d_stride=(2, 2),
                     pad="SAME",
                     dtype=tf.float32,
                     collections=None,
@@ -146,9 +146,19 @@ def conv_2d_network(x,
     Returns:
         tensor holding state features;
     """
-    for i in range(num_layers):
+    for i in range(conv_2d_num_layers):
         x = tf.nn.elu(
-            conv2d(x, num_filters, "conv2d_{}".format(i + 1), filter_size, stride, pad, dtype, collections, reuse)
+            conv2d(
+                x,
+                conv_2d_num_filters,
+                "conv2d_{}".format(i + 1),
+                conv_2d_filter_size,
+                conv_2d_stride,
+                pad,
+                dtype,
+                collections,
+                reuse
+            )
         )
     # A3c/BaseAAC original paper design:
     # x = tf.nn.elu(conv2d(x, 16, 'conv2d_1', [8, 8], [4, 4], pad, dtype, collections, reuse))
@@ -159,7 +169,42 @@ def conv_2d_network(x,
     return x
 
 
-def lstm_network(x, a_r, sequence_length, lstm_class=rnn.BasicLSTMCell, lstm_layers=(256,), reuse=False):
+def conv_1d_network(x,
+                    ob_space,
+                    ac_space,
+                    conv_1d_num_layers=4,
+                    conv_1d_num_filters=32,
+                    conv_1d_filter_size=3,
+                    conv_1d_stride=2,
+                    pad="SAME",
+                    dtype=tf.float32,
+                    collections=None,
+                    reuse=False):
+    """
+    Stage1 network: from preprocessed 1D input to estimated features.
+    Encapsulates convolutions, [possibly] skip-connections etc. Can be shared.
+
+    Returns:
+        tensor holding state features;
+    """
+    for i in range(conv_1d_num_layers):
+        x = tf.nn.elu(
+            conv1d(
+                x,
+                conv_1d_num_filters,
+                "conv1d_{}".format(i + 1),
+                conv_1d_filter_size,
+                conv_1d_stride,
+                pad,
+                dtype,
+                collections,
+                reuse
+            )
+        )
+    return x
+
+
+def lstm_network(x, lstm_sequence_length, lstm_class=rnn.BasicLSTMCell, lstm_layers=(256,), reuse=False):
     """
     Stage2 network: from features to flattened LSTM output.
     Defines [multi-layered] dynamic [possibly shared] LSTM network.
@@ -172,7 +217,7 @@ def lstm_network(x, a_r, sequence_length, lstm_class=rnn.BasicLSTMCell, lstm_lay
     """
     with tf.variable_scope('lstm', reuse=reuse):
         # Flatten, add action/reward and expand with fake [time] batch? dim to feed LSTM bank:
-        x = tf.concat([x, a_r] ,axis=-1)
+        #x = tf.concat([x, a_r] ,axis=-1)
         #x = tf.concat([batch_flatten(x), a_r], axis=-1)
         #x = tf.expand_dims(x, [0])
 
@@ -194,7 +239,7 @@ def lstm_network(x, a_r, sequence_length, lstm_class=rnn.BasicLSTMCell, lstm_lay
             lstm,
             x,
             initial_state=lstm_state_pl,
-            sequence_length=sequence_length,
+            sequence_length=lstm_sequence_length,
             time_major=False
         )
         #x_out = tf.reshape(lstm_outputs, [-1, lstm_layers[-1]])

btgym/algorithms/policy.py
@@ -24,8 +24,14 @@ class BaseAacPolicy(object):
     https://arxiv.org/abs/1611.05397
     """
 
-    def __init__(self, ob_space, ac_space, rp_sequence_size,
-                 lstm_class=rnn.BasicLSTMCell, lstm_layers=(256,), aux_estimate=True, **kwargs):
+    def __init__(self,
+                 ob_space,
+                 ac_space,
+                 rp_sequence_size,
+                 lstm_class=rnn.BasicLSTMCell,
+                 lstm_layers=(256,),
+                 aux_estimate=True,
+                 **kwargs):
         """
         Defines [partially shared] on/off-policy networks for estimating  action-logits, value function,
         reward and state 'pixel_change' predictions.
@@ -66,7 +72,7 @@ class BaseAacPolicy(object):
 
         # Base on-policy AAC network:
         # Conv. layers:
-        on_aac_x = conv_2d_network(self.on_state_in['external'], ob_space['external'], ac_space)
+        on_aac_x = conv_2d_network(self.on_state_in['external'], ob_space['external'], ac_space, **kwargs)
 
         # Reshape rnn inputs for  batch training as [rnn_batch_dim, rnn_time_dim, flattened_depth]:
         x_shape_dynamic = tf.shape(on_aac_x)
@@ -76,19 +82,32 @@ class BaseAacPolicy(object):
         on_a_r_in = tf.reshape(self.on_a_r_in, [self.on_batch_size, max_seq_len, ac_space + 1])
         on_aac_x = tf.reshape( on_aac_x, [self.on_batch_size, max_seq_len, np.prod(x_shape_static[1:])])
 
+        # Feed last action_reward [, internal obs. state] into LSTM along with external state features:
+        on_stage2_input = [on_aac_x, on_a_r_in]
+
+        if 'internal' in list(self.on_state_in.keys()):
+            x_int_shape_static = self.on_state_in['internal'].get_shape().as_list()
+            x_int = tf.reshape(
+                self.on_state_in['internal'],
+                [self.on_batch_size, max_seq_len, np.prod(x_int_shape_static[1:])]
+            )
+            on_stage2_input.append(x_int)
+
+        on_aac_x = tf.concat(on_stage2_input, axis=-1)
+
         # LSTM layer takes conv. features and concatenated last action_reward tensor:
         [on_x_lstm_out, self.on_lstm_init_state, self.on_lstm_state_out, self.on_lstm_state_pl_flatten] =\
-            lstm_network(on_aac_x, on_a_r_in, self.on_time_length, lstm_class, lstm_layers, )
+            lstm_network(on_aac_x, self.on_time_length, lstm_class, lstm_layers)
 
         # Reshape back to [batch, flattened_depth], where batch = rnn_batch_dim * rnn_time_dim:
         x_shape_static = on_x_lstm_out.get_shape().as_list()
         on_x_lstm_out = tf.reshape(on_x_lstm_out, [x_shape_dynamic[0], x_shape_static[-1]])
 
-        # aac policy and value outputs and action-sampling function:
+        # Aac policy and value outputs and action-sampling function:
         [self.on_logits, self.on_vf, self.on_sample] = dense_aac_network(on_x_lstm_out, ac_space)
 
         # Off-policy AAC network (shared):
-        off_aac_x = conv_2d_network(self.off_state_in['external'], ob_space['external'], ac_space, reuse=True)
+        off_aac_x = conv_2d_network(self.off_state_in['external'], ob_space['external'], ac_space, reuse=True, **kwargs)
 
         # Reshape rnn inputs for  batch training as [rnn_batch_dim, rnn_time_dim, flattened_depth]:
         x_shape_dynamic = tf.shape(off_aac_x)
@@ -98,8 +117,20 @@ class BaseAacPolicy(object):
         off_a_r_in = tf.reshape(self.off_a_r_in, [self.off_batch_size, max_seq_len, ac_space + 1])
         off_aac_x = tf.reshape( off_aac_x, [self.off_batch_size, max_seq_len, np.prod(x_shape_static[1:])])
 
+        off_stage2_input = [off_aac_x, off_a_r_in]
+
+        if 'internal' in list(self.off_state_in.keys()):
+            x_int_shape_static = self.off_state_in['internal'].get_shape().as_list()
+            off_x_int = tf.reshape(
+                self.off_state_in['internal'],
+                [self.off_batch_size, max_seq_len, np.prod(x_int_shape_static[1:])]
+            )
+            off_stage2_input.append(off_x_int)
+
+        off_aac_x = tf.concat(off_stage2_input, axis=-1)
+
         [off_x_lstm_out, _, _, self.off_lstm_state_pl_flatten] =\
-            lstm_network(off_aac_x, off_a_r_in, self.off_time_length, lstm_class, lstm_layers, reuse=True)
+            lstm_network(off_aac_x, self.off_time_length, lstm_class, lstm_layers, reuse=True)
 
         # Reshape back to [batch, flattened_depth], where batch = rnn_batch_dim * rnn_time_dim:
         x_shape_static = off_x_lstm_out.get_shape().as_list()
@@ -143,7 +174,7 @@ class BaseAacPolicy(object):
         self.rp_batch_size = tf.placeholder(tf.int32, name='rp_batch_size')
 
         # Shared conv. output:
-        rp_x = conv_2d_network(self.rp_state_in['external'], ob_space['external'], ac_space, reuse=True)
+        rp_x = conv_2d_network(self.rp_state_in['external'], ob_space['external'], ac_space, reuse=True, **kwargs)
 
         # Flatten batch-wise:
         rp_x_shape_static = rp_x.get_shape().as_list()
@@ -253,3 +284,191 @@ class BaseAacPolicy(object):
 
         return sess.run(self.pc_target, feeder)[0,...,0]
 
+
+class Aac1dPolicy(BaseAacPolicy):
+    """
+    AAC policy for one-dimensional signal obs. state.
+    """
+
+    def __init__(self,
+                 ob_space,
+                 ac_space,
+                 rp_sequence_size,
+                 lstm_class=rnn.BasicLSTMCell,
+                 lstm_layers=(256,),
+                 aux_estimate=False,
+                 **kwargs):
+        """
+        Defines [partially shared] on/off-policy networks for estimating  action-logits, value function,
+        reward and state 'pixel_change' predictions.
+        Expects uni-modal observation as array of shape `ob_space`.
+
+        Args:
+            ob_space:           dictionary of observation state shapes
+            ac_space:           discrete action space shape (length)
+            rp_sequence_size:   reward prediction sample length
+            lstm_class:         tf.nn.lstm class
+            lstm_layers:        tuple of LSTM layers sizes
+            aux_estimate:       (bool), if True - add auxiliary tasks estimations to self.callbacks dictionary.
+            **kwargs            not used
+        """
+        self.ob_space = ob_space
+        self.ac_space = ac_space
+        self.rp_sequence_size = rp_sequence_size
+        self.lstm_class = lstm_class
+        self.lstm_layers = lstm_layers
+        self.aux_estimate = aux_estimate
+        self.callback = {}
+
+        # Placeholders for obs. state input:
+        self.on_state_in = nested_placeholders(ob_space, batch_dim=None, name='on_policy_state_in')
+        self.off_state_in = nested_placeholders(ob_space, batch_dim=None, name='off_policy_state_in_pl')
+        self.rp_state_in = nested_placeholders(ob_space, batch_dim=None, name='rp_state_in')
+
+        # Placeholders for concatenated action [one-hot] and reward [scalar]:
+        self.on_a_r_in = tf.placeholder(tf.float32, [None, ac_space + 1], name='on_policy_action_reward_in_pl')
+        self.off_a_r_in = tf.placeholder(tf.float32, [None, ac_space + 1], name='off_policy_action_reward_in_pl')
+
+        # Placeholders for rnn batch and time-step dimensions:
+        self.on_batch_size = tf.placeholder(tf.int32, name='on_policy_batch_size')
+        self.on_time_length = tf.placeholder(tf.int32, name='on_policy_sequence_size')
+
+        self.off_batch_size = tf.placeholder(tf.int32, name='off_policy_batch_size')
+        self.off_time_length = tf.placeholder(tf.int32, name='off_policy_sequence_size')
+
+        # Base on-policy AAC network:
+        # Conv. layers:
+        on_aac_x = conv_1d_network(self.on_state_in['external'], ob_space['external'], ac_space, **kwargs)
+
+        # Reshape rnn inputs for  batch training as [rnn_batch_dim, rnn_time_dim, flattened_depth]:
+        x_shape_dynamic = tf.shape(on_aac_x)
+        max_seq_len = tf.cast(x_shape_dynamic[0] / self.on_batch_size, tf.int32)
+        x_shape_static = on_aac_x.get_shape().as_list()
+
+        on_a_r_in = tf.reshape(self.on_a_r_in, [self.on_batch_size, max_seq_len, ac_space + 1])
+        on_aac_x = tf.reshape( on_aac_x, [self.on_batch_size, max_seq_len, np.prod(x_shape_static[1:])])
+
+        # Feed last action_reward [, internal obs. state] into LSTM along with external state features:
+        on_stage2_input = [on_aac_x, on_a_r_in]
+
+        if 'internal' in list(self.on_state_in.keys()):
+            x_int_shape_static = self.on_state_in['internal'].get_shape().as_list()
+            x_int = tf.reshape(
+                self.on_state_in['internal'],
+                [self.on_batch_size, max_seq_len, np.prod(x_int_shape_static[1:])]
+            )
+            on_stage2_input.append(x_int)
+
+        on_aac_x = tf.concat(on_stage2_input, axis=-1)
+
+        # LSTM layer takes conv. features and concatenated last action_reward tensor:
+        [on_x_lstm_out, self.on_lstm_init_state, self.on_lstm_state_out, self.on_lstm_state_pl_flatten] =\
+            lstm_network(on_aac_x, self.on_time_length, lstm_class, lstm_layers)
+
+        # Reshape back to [batch, flattened_depth], where batch = rnn_batch_dim * rnn_time_dim:
+        x_shape_static = on_x_lstm_out.get_shape().as_list()
+        on_x_lstm_out = tf.reshape(on_x_lstm_out, [x_shape_dynamic[0], x_shape_static[-1]])
+
+        # Aac policy and value outputs and action-sampling function:
+        [self.on_logits, self.on_vf, self.on_sample] = dense_aac_network(on_x_lstm_out, ac_space)
+
+        # Off-policy AAC network (shared):
+        off_aac_x = conv_1d_network(self.off_state_in['external'], ob_space['external'], ac_space, reuse=True, **kwargs)
+
+        # Reshape rnn inputs for  batch training as [rnn_batch_dim, rnn_time_dim, flattened_depth]:
+        x_shape_dynamic = tf.shape(off_aac_x)
+        max_seq_len = tf.cast(x_shape_dynamic[0] / self.off_batch_size, tf.int32)
+        x_shape_static = off_aac_x.get_shape().as_list()
+
+        off_a_r_in = tf.reshape(self.off_a_r_in, [self.off_batch_size, max_seq_len, ac_space + 1])
+        off_aac_x = tf.reshape( off_aac_x, [self.off_batch_size, max_seq_len, np.prod(x_shape_static[1:])])
+
+        off_stage2_input = [off_aac_x, off_a_r_in]
+
+        if 'internal' in list(self.off_state_in.keys()):
+            x_int_shape_static = self.off_state_in['internal'].get_shape().as_list()
+            off_x_int = tf.reshape(
+                self.off_state_in['internal'],
+                [self.off_batch_size, max_seq_len, np.prod(x_int_shape_static[1:])]
+            )
+            off_stage2_input.append(off_x_int)
+
+        off_aac_x = tf.concat(off_stage2_input, axis=-1)
+
+        [off_x_lstm_out, _, _, self.off_lstm_state_pl_flatten] =\
+            lstm_network(off_aac_x, self.off_time_length, lstm_class, lstm_layers, reuse=True)
+
+        # Reshape back to [batch, flattened_depth], where batch = rnn_batch_dim * rnn_time_dim:
+        x_shape_static = off_x_lstm_out.get_shape().as_list()
+        off_x_lstm_out = tf.reshape(off_x_lstm_out, [x_shape_dynamic[0], x_shape_static[-1]])
+
+        [self.off_logits, self.off_vf, _] =\
+            dense_aac_network(off_x_lstm_out, ac_space, reuse=True)
+
+        # Aux1: `Pixel control` network:
+        # Define pixels-change estimation function:
+        # Yes, it rather env-specific but for atari case it is handy to do it here, see self.get_pc_target():
+        [self.pc_change_state_in, self.pc_change_last_state_in, self.pc_target] = [None,None,None] #\
+            #pixel_change_2d_estimator(ob_space['external'])
+
+        self.pc_batch_size = self.off_batch_size
+        self.pc_time_length = self.off_time_length
+
+        self.pc_state_in = self.off_state_in
+        self.pc_a_r_in = self.off_a_r_in
+        self.pc_lstm_state_pl_flatten = self.off_lstm_state_pl_flatten
+
+        # Shared conv and lstm nets, same off-policy batch:
+        #pc_x = off_x_lstm_out
+
+        # PC duelling Q-network, outputs [None, 20, 20, ac_size] Q-features tensor:
+        #self.pc_q = duelling_pc_network(pc_x, self.ac_space)
+
+        # Aux2: `Value function replay` network:
+        # VR network is fully shared with ppo network but with `value` only output:
+        # and has same off-policy batch pass with off_ppo network:
+        self.vr_batch_size = self.off_batch_size
+        self.vr_time_length = self.off_time_length
+
+        self.vr_state_in = self.off_state_in
+        self.vr_a_r_in = self.off_a_r_in
+
+        self.vr_lstm_state_pl_flatten = self.off_lstm_state_pl_flatten
+        self.vr_value = self.off_vf
+
+        # Aux3: `Reward prediction` network:
+        #self.rp_batch_size = tf.placeholder(tf.int32, name='rp_batch_size')
+
+        # Shared conv. output:
+        #rp_x = conv_2d_network(self.rp_state_in['external'], ob_space['external'], ac_space, reuse=True, **kwargs)
+
+        # Flatten batch-wise:
+        #rp_x_shape_static = rp_x.get_shape().as_list()
+        #rp_x = tf.reshape(rp_x, [self.rp_batch_size, np.prod(rp_x_shape_static[1:]) * (self.rp_sequence_size-1)])
+
+        # RP output:
+        #self.rp_logits = dense_rp_network(rp_x)
+
+        # Batch-norm related (useless, ignore):
+        try:
+            if self.train_phase is not None:
+                pass
+
+        except:
+            self.train_phase = tf.placeholder_with_default(
+                tf.constant(False, dtype=tf.bool),
+                shape=(),
+                name='train_phase_flag_pl'
+            )
+        self.update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        # Add moving averages to save list:
+        moving_var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, tf.get_variable_scope().name + '.*moving.*')
+        renorm_var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, tf.get_variable_scope().name + '.*renorm.*')
+
+        # What to save:
+        self.var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
+        self.var_list += moving_var_list + renorm_var_list
+
+        # Callbacks:
+        if self.aux_estimate:
+            self.callback['pixel_change'] = self.get_pc_target

btgym/algorithms/runner.py
@@ -144,18 +144,18 @@ def env_runner(sess,
     last_context = policy.get_initial_features()
     length = 0
     local_episode = 0
-    rewards = 0
+    reward_sum = 0
     last_action = np.zeros(env.action_space.n)
     last_action[0] = 1
     last_reward = 0.0
     last_action_reward = np.concatenate([last_action, np.asarray([last_reward])], axis=-1)
 
     # Summary averages accumulators:
-    total_r = 0
-    cpu_time = 0
-    final_value = 0
-    total_steps = 0
-    total_steps_atari = 0
+    total_r = []
+    cpu_time = []
+    final_value = []
+    total_steps = []
+    total_steps_atari = []
 
     ep_stat = None
     render_stat = None
@@ -188,7 +188,7 @@ def env_runner(sess,
             last_experience[key] = callback(**locals())
 
         length += 1
-        rewards += reward
+        reward_sum += reward
         last_state = state
         last_context = context
         last_action = action
@@ -227,7 +227,7 @@ def env_runner(sess,
 
                 # Housekeeping:
                 length += 1
-                rewards += reward
+                reward_sum += reward
                 last_state = state
                 last_context = context
                 last_action = action
@@ -241,36 +241,36 @@ def env_runner(sess,
                 # All environment-specific summaries are here due to fact
                 # only runner allowed to interact with environment:
                 # Accumulate values for averaging:
-                total_r += rewards
-                total_steps_atari += length
+                total_r += [reward_sum]
+                total_steps_atari += [length]
                 if not test:
                     episode_stat = env.get_stat()  # get episode statistic
-                    last_i = info[0]  # pull most recent info
-                    cpu_time += episode_stat['runtime'].total_seconds()
-                    final_value += last_i['broker_value']
-                    total_steps += episode_stat['length']
+                    last_i = info[-1]  # pull most recent info
+                    cpu_time += [episode_stat['runtime'].total_seconds()]
+                    final_value += [last_i['broker_value']]
+                    total_steps += [episode_stat['length']]
 
                 # Episode statistic:
                 if local_episode % episode_summary_freq == 0:
                     if not test:
                         # BTgym:
                         ep_stat = dict(
-                            total_r=total_r / episode_summary_freq,
-                            cpu_time=cpu_time / episode_summary_freq,
-                            final_value=final_value / episode_summary_freq,
-                            steps=total_steps / episode_summary_freq
+                            total_r=np.average(total_r),
+                            cpu_time=np.average(cpu_time),
+                            final_value=np.average(final_value),
+                            steps=np.average(total_steps)
                         )
                     else:
                         # Atari:
                         ep_stat = dict(
-                            total_r=total_r / episode_summary_freq,
-                            steps=total_steps_atari / episode_summary_freq
+                            total_r=np.average(total_r),
+                            steps=np.average(total_steps_atari)
                         )
-                    total_r = 0
-                    cpu_time = 0
-                    final_value = 0
-                    total_steps = 0
-                    total_steps_atari = 0
+                    total_r = []
+                    cpu_time = []
+                    final_value = []
+                    total_steps = []
+                    total_steps_atari = []
 
                 if task == 0 and local_episode % env_render_freq == 0 :
                     if not test:
@@ -278,7 +278,7 @@ def env_runner(sess,
 
                         render_stat = dict(
                             render_human=env.render('human')[None,:],
-                            render_model_input=env.render('model_input')[None, :],
+                            render_model_input_ext=env.render('external')[None, :],
                             render_episode=env.render('episode')[None,:],
                         )
                     else:
@@ -292,7 +292,7 @@ def env_runner(sess,
 
                 last_context = policy.get_initial_features()
                 length = 0
-                rewards = 0
+                reward_sum = 0
                 last_action = np.zeros(env.action_space.n)
                 last_action[0] = 1
                 last_reward = 0.0

btgym/research/strategy_4.py
@@ -31,10 +31,6 @@ class DevStrat_4_6(BTgymBaseStrategy):
 
     Data:
         synthetic/real
-
-    Note:
-
-        call classmethod set_params() before use!
     """
     time_dim = 30
     params = dict(
@@ -137,7 +133,6 @@ class DevStrat_4_6(BTgymBaseStrategy):
             ],
             axis=-1
         )
-        #self.state['raw_state'] = self.raw_state
         return self.state
 
     def get_reward(self):
@@ -154,7 +149,7 @@ class DevStrat_4_6(BTgymBaseStrategy):
 
         # Check if any trades been closed in given period:
         realized_pnl = np.asarray(self.sliding_stat['realized_pnl'])
-        is_result = realized_pnl!= 0
+        is_result = realized_pnl != 0
 
         # If yes - compute averaged result:
         if is_result.any():
@@ -188,9 +183,9 @@ class DevStrat_4_6(BTgymBaseStrategy):
 
         # Weights are subject to tune:
         self.reward = (
-            + 1.0 * avg_unrealised_pnl * max_to_real_k  # TODO: make it zero when pos.size=0
-            + 0.01 * avg_norm_broker_value
-            + 10.0 * avg_realized_pnl * max_to_real_k
+            + 1.0 * avg_unrealised_pnl #* max_to_real_k  # TODO: make it zero when pos.size=0
+            + 0.1 * avg_norm_broker_value
+            + 100.0 * avg_realized_pnl #* max_to_real_k
             # 'Close-at-the-end' term:
             # - 1.0 * self.exp_scale(avg_norm_episode_duration, gamma=6) * abs_max_norm_exposure
             # 'Do-not-expose-for-too-long' term: